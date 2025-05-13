397189782ea734e76019b8f3ce2a966dfff90517
lostinaa <lostinaa@github.com>
2025-05-13 10:46:06 +0000

    initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+/*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'RESEARCH'  | 'IT' | 'ADMIN' ) 
+  {
+    return []
+  }
+ 
+
+
+
+@Get(':id')
+findOne(@Param('id') id: string) {
+    return{id}
+}
+
+
+ @Post()
+ create(@Body() user: {}){
+    return user;
+ }
+
+ @Patch(':id')  //patch /user/:id
+update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return{id, ...userUpdate}
+}
+
+@Delete(':id')
+delete(@Param('id') id: string) {
+    return{id}
+}
+}
+ 
